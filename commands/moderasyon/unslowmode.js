400b07ec38867211a7992e878cc7ceb6aa70d736
ForsDev101 <forsdeving@gmail.com>
2025-08-02 00:07:03 +0300

    Update unslowmode.js

--- commands/moderasyon/unslowmode.js
+++ commands/moderasyon/unslowmode.js
@@ -1,24 +1,32 @@
 const { SlashCommandBuilder, PermissionFlagsBits, EmbedBuilder } = require('discord.js');
+const { rpgEmbed } = require('../../utils/embedRPG');
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('unslowmode')
-    .setDescription('Kanalın yavaş modunu sıfırlar (kaldırır).')
+    .setDescription('Kanaldaki yavaş modu kaldırır')
     .setDefaultMemberPermissions(PermissionFlagsBits.ManageChannels),
 
   async execute(interaction) {
     try {
+      await interaction.deferReply();
+
       await interaction.channel.setRateLimitPerUser(0);
+
       const embed = new EmbedBuilder()
-        .setColor('Green')
-        .setTitle('Yavaş Mod Kaldırıldı')
+        .setTitle('🐇 Slowmode Kaldırıldı')
         .setDescription(`${interaction.channel} kanalındaki yavaş mod kaldırıldı.`)
+        .setFooter({ text: `İşlemi yapan: ${interaction.user.tag}`, iconURL: interaction.user.displayAvatarURL() })
         .setTimestamp();
 
-      await interaction.reply({ embeds: [embed] });
-    } catch (error) {
-      console.error(error);
-      interaction.reply({ content: 'Yavaş mod kaldırılırken hata oluştu.', ephemeral: true });
+      await rpgEmbed(interaction, embed, 500);
+    } catch (err) {
+      console.error(err);
+      if (interaction.deferred || interaction.replied) {
+        await interaction.followUp({ content: 'Komut çalıştırılırken hata oluştu.', ephemeral: true });
+      } else {
+        await interaction.reply({ content: 'Komut çalıştırılırken hata oluştu.', ephemeral: true });
+      }
     }
   }
 };
